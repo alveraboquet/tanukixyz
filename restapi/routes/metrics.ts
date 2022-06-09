ce9a1c9d466c612f7727a5982117ac2426b8f715
Noat <noat.eth@gmail.com>
2022-06-09 23:08:22 +0700

    add api query date data endpoint

--- restapi/routes/metrics.ts
+++ restapi/routes/metrics.ts
@@ -5,6 +5,7 @@ import logger from '../../core/logger';
 import { ShareProviders } from '../../core/types';
 import { getCache, setCache } from '../caching';
 import { removeIdFields, writeResponseData, writeResponseError } from '../helpers';
+import {getTodayUTCTimestamp} from "../../core/helper";
 
 export function getRouter(providers: ShareProviders): Router {
   const router = Router({ mergeParams: true });
@@ -58,6 +59,57 @@ export function getRouter(providers: ShareProviders): Router {
       });
     }
   });
+
+  router.get('/date/:module', async (request, response) => {
+    const module = request.params.module || 'unknown';
+
+    let toDate = getTodayUTCTimestamp();
+    let fromDate = toDate - 7 * 24 * 60 * 60; // last 7 days
+
+    // fromDate and toDate format: 2022-11-30
+    if (request.query['fromDate']) {
+      fromDate = Math.floor(new Date(request.query['fromDate'].toString()).getTime() / 1000);
+    }
+    if (request.query['toDate']) {
+      toDate = Math.floor(new Date(request.query['toDate'].toString()).getTime() / 1000);
+    }
+
+    const dateMetricCollection = await providers.database.getCollection(
+      envConfig.database.collections.globalDataDate
+    );
+
+    try {
+      const documents: Array<any> = await dateMetricCollection
+        .find({
+          module: module,
+          date: {
+            $gte: fromDate,
+            $lte: toDate,
+          },
+        })
+        .sort({date: -1})
+        .toArray();
+      writeResponseData(response, {
+        status: 200,
+        data: documents,
+        removeIdField: true,
+      });
+    } catch (e: any) {
+      logger.onError({
+        source: 'router.metrics',
+        message: 'failed to serve api request',
+        props: {
+          path: request.path,
+          error: e.message,
+        },
+        error: e as Error,
+      });
+      writeResponseError(response, {
+        status: 500,
+        error: e.message,
+      });
+    }
+  })
 
   return router;
 }
