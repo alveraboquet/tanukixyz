734946d17073cbd24f82d82ab8a0656d250e09ed
Noat <noat.eth@gmail.com>
2022-07-22 14:12:05 +0700

    disable address snapshot sync

--- modules/registry/providers/registry.ts
+++ modules/registry/providers/registry.ts
@@ -41,54 +41,54 @@ class RegistryProvider implements Provider {
     await this.syncSnapshotAddressData({ providers, timestamp: getTimestamp(), snapshot: false });
   }
 
-  private async _syncSnapshotInfo(props: StartRegistryServiceProps): Promise<void> {
-    const { providers, forceSync } = props;
-
-    const stateCollection = await providers.database.getCollection(envConfig.database.collections.globalState);
-
-    let startDate = InitialSyncDate;
-    if (!forceSync) {
-      const latestSyncData = await stateCollection
-        .find({
-          name: `registry-address-${this.configs.name}`,
-        })
-        .sort({ date: -1 })
-        .limit(1)
-        .toArray();
-      if (latestSyncData.length > 0) {
-        startDate = latestSyncData[0].timestamp > startDate ? latestSyncData[0].timestamp : startDate;
-      }
-    }
-
-    logger.onInfo({
-      source: this.name,
-      message: 'collecting address snapshot data',
-      props: {
-        name: (this.configs as RegistryProtocolConfig).name,
-        fromTime: startDate,
-      },
-    });
-
-    const currentTimestamp = getTimestamp();
-    while (startDate <= currentTimestamp) {
-      await this.syncSnapshotAddressData({ providers, timestamp: startDate, snapshot: true });
-
-      await stateCollection.updateOne(
-        {
-          name: `registry-address-${this.configs.name}`,
-        },
-        {
-          $set: {
-            name: `registry-address-${this.configs.name}`,
-            timestamp: startDate,
-          },
-        },
-        { upsert: true }
-      );
-
-      startDate += 24 * 60 * 60;
-    }
-  }
+  // private async _syncSnapshotInfo(props: StartRegistryServiceProps): Promise<void> {
+  //   const { providers, forceSync } = props;
+  //
+  //   const stateCollection = await providers.database.getCollection(envConfig.database.collections.globalState);
+  //
+  //   let startDate = InitialSyncDate;
+  //   if (!forceSync) {
+  //     const latestSyncData = await stateCollection
+  //       .find({
+  //         name: `registry-address-${this.configs.name}`,
+  //       })
+  //       .sort({ date: -1 })
+  //       .limit(1)
+  //       .toArray();
+  //     if (latestSyncData.length > 0) {
+  //       startDate = latestSyncData[0].timestamp > startDate ? latestSyncData[0].timestamp : startDate;
+  //     }
+  //   }
+  //
+  //   logger.onInfo({
+  //     source: this.name,
+  //     message: 'collecting address snapshot data',
+  //     props: {
+  //       name: (this.configs as RegistryProtocolConfig).name,
+  //       fromTime: startDate,
+  //     },
+  //   });
+  //
+  //   const currentTimestamp = getTimestamp();
+  //   while (startDate <= currentTimestamp) {
+  //     await this.syncSnapshotAddressData({ providers, timestamp: startDate, snapshot: true });
+  //
+  //     await stateCollection.updateOne(
+  //       {
+  //         name: `registry-address-${this.configs.name}`,
+  //       },
+  //       {
+  //         $set: {
+  //           name: `registry-address-${this.configs.name}`,
+  //           timestamp: startDate,
+  //         },
+  //       },
+  //       { upsert: true }
+  //     );
+  //
+  //     startDate += 24 * 60 * 60;
+  //   }
+  // }
 
   public async startService(props: StartRegistryServiceProps): Promise<void> {
     await this._syncCurrentInfo(props);
