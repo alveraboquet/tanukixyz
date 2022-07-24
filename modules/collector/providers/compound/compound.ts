50efcc4b1b645d7df460e04d03dcdc26b5825ce9
Noat <noat.eth@gmail.com>
2022-07-24 23:27:49 +0700

    fix: compound underlying price

--- modules/collector/providers/compound/compound.ts
+++ modules/collector/providers/compound/compound.ts
@@ -70,7 +70,7 @@ export class CompoundProvider extends CollectorProvider {
         if (historyPrices[poolConfig.underlying.coingeckoId]) {
           historyPrice = historyPrices[poolConfig.underlying.coingeckoId];
         } else {
-          historyPrice = await getHistoryTokenPriceFromCoingecko(poolConfig.underlying.coingeckoId, toTime);
+          historyPrice = await getHistoryTokenPriceFromCoingecko(poolConfig.underlying.coingeckoId, fromTime);
           historyPrices[poolConfig.underlying.coingeckoId] = historyPrice;
         }
       } else {
@@ -134,29 +134,29 @@ export class CompoundProvider extends CollectorProvider {
               .multipliedBy(historyPrice)
               .toNumber();
 
-            try {
-              // count revenue = totalBorrowAfterWithRepay - totalBorrowBeforeRepay
-              const contract = new web3.eth.Contract(CompoundLendAbi as any, poolConfig.contractAddress);
-              const totalBorrows = await contract.methods.totalBorrows().call(events[i].blockNumber - 1);
-              const multiplier = new BigNumber(10).pow(
-                poolConfig.underlying.chains[poolConfig.chainConfig.name].decimals
-              );
-              const borrowBefore = new BigNumber(totalBorrows.toString()).dividedBy(multiplier).toNumber();
-              const borrowAfter = new BigNumber(events[i].returnValues.totalBorrows).dividedBy(multiplier).toNumber();
-              const repayAmount = new BigNumber(events[i].returnValues.repayAmount).dividedBy(multiplier).toNumber();
-              data.revenueUSD +=
-                repayAmount + borrowAfter > borrowBefore ? repayAmount + borrowAfter - borrowBefore : 0;
-            } catch (e: any) {
-              logger.onDebug({
-                source: this.name,
-                message: 'cannot query history data',
-                props: {
-                  chain: poolConfig.chainConfig.name,
-                  contract: normalizeAddress(poolConfig.contractAddress),
-                  error: e.message,
-                },
-              });
-            }
+            // try {
+            //   // count revenue = totalBorrowAfterWithRepay - totalBorrowBeforeRepay
+            //   const contract = new web3.eth.Contract(CompoundLendAbi as any, poolConfig.contractAddress);
+            //   const totalBorrows = await contract.methods.totalBorrows().call(events[i].blockNumber - 1);
+            //   const multiplier = new BigNumber(10).pow(
+            //     poolConfig.underlying.chains[poolConfig.chainConfig.name].decimals
+            //   );
+            //   const borrowBefore = new BigNumber(totalBorrows.toString()).dividedBy(multiplier).toNumber();
+            //   const borrowAfter = new BigNumber(events[i].returnValues.totalBorrows).dividedBy(multiplier).toNumber();
+            //   const repayAmount = new BigNumber(events[i].returnValues.repayAmount).dividedBy(multiplier).toNumber();
+            //   data.revenueUSD +=
+            //     repayAmount + borrowAfter > borrowBefore ? repayAmount + borrowAfter - borrowBefore : 0;
+            // } catch (e: any) {
+            //   logger.onDebug({
+            //     source: this.name,
+            //     message: 'cannot query history data',
+            //     props: {
+            //       chain: poolConfig.chainConfig.name,
+            //       contract: normalizeAddress(poolConfig.contractAddress),
+            //       error: e.message,
+            //     },
+            //   });
+            // }
           }
         }
       }
