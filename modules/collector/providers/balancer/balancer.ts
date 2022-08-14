584471b9ca1f47c5606a9fef0f6b0cccee7f6a3e
Noat <noat.eth@gmail.com>
2022-08-14 16:19:24 +0700

    format balancer collector

--- modules/collector/providers/balancer/balancer.ts
+++ modules/collector/providers/balancer/balancer.ts
@@ -12,8 +12,13 @@ export class BalancerProvider extends CollectorProvider {
     super(configs);
   }
 
-  public async getDataInTimeFrame(providers: ShareProviders, fromTime: number, toTime: number): Promise<ProtocolData> {
-    const dateData: ProtocolData = {
+  private async getOverviewData(
+    providers: ShareProviders,
+    subgraph: any,
+    fromBlock: number,
+    toBlock: number
+  ): Promise<ProtocolData> {
+    const data: ProtocolData = {
       revenueUSD: 0,
       totalValueLockedUSD: 0,
       volumeInUseUSD: 0,
@@ -21,32 +26,18 @@ export class BalancerProvider extends CollectorProvider {
       transactionCount: 0,
     };
 
-    for (let i = 0; i < this.configs.subgraphs.length; i++) {
-      const transactionCountFilter = `${this.configs.subgraphs[i].version === 1 ? 'txCount' : 'totalSwapCount'}`;
-      const blockNumberStartDate = await providers.subgraph.queryBlockAtTimestamp(
-        this.configs.subgraphs[i].chainConfig.subgraph?.blockSubgraph as string,
-        fromTime
-      );
-      let blockNumberEndDate = await providers.subgraph.queryBlockAtTimestamp(
-        this.configs.subgraphs[i].chainConfig.subgraph?.blockSubgraph as string,
-        toTime
-      );
-
-      // in case subgraph not full sync yet
-      const blockNumberMeta = await providers.subgraph.queryMetaLatestBlock(this.configs.subgraphs[i].exchange);
-      blockNumberEndDate = blockNumberEndDate > blockNumberMeta ? blockNumberMeta : blockNumberEndDate;
-
-      const response = await providers.subgraph.querySubgraph(
-        this.configs.subgraphs[i].exchange,
-        `
+    const transactionCountFilter = `${subgraph.version === 1 ? 'txCount' : 'totalSwapCount'}`;
+    const response = await providers.subgraph.querySubgraph(
+      subgraph.exchange,
+      `
           {
-            startDate: balancers(first: 1, block: {number: ${blockNumberStartDate}}) {
+            startDate: balancers(first: 1, block: {number: ${fromBlock}}) {
               totalSwapFee
               totalSwapVolume
               ${transactionCountFilter}
               totalLiquidity
             }
-            endDate: balancers(first: 1, block: {number: ${blockNumberEndDate}}) {
+            endDate: balancers(first: 1, block: {number: ${toBlock}}) {
               totalSwapFee
               totalSwapVolume
               ${transactionCountFilter}
@@ -54,77 +45,67 @@ export class BalancerProvider extends CollectorProvider {
             }
           }
         `
-      );
-      if (
-        response['startDate'] &&
-        response['startDate'].length > 0 &&
-        response['endDate'] &&
-        response['endDate'].length > 0
-      ) {
-        dateData.revenueUSD +=
-          Number(response['endDate'][0]['totalSwapFee']) - Number(response['startDate'][0]['totalSwapFee']);
-        dateData.volumeInUseUSD +=
-          Number(response['endDate'][0]['totalSwapVolume']) - Number(response['startDate'][0]['totalSwapVolume']);
-        dateData.transactionCount +=
-          Number(response['endDate'][0][transactionCountFilter]) -
-          Number(response['startDate'][0][transactionCountFilter]);
-        dateData.totalValueLockedUSD +=
-          this.configs.subgraphs[i].version === 1
-            ? new BigNumber(response['endDate'][0]['totalLiquidity']).dividedBy(1e18).toNumber()
-            : Number(response['endDate'][0]['totalLiquidity']);
-      }
+    );
 
-      // count user
-      // try {
-      //   const addresses: any = {};
-      //   let startTime = fromTime;
-      //   while (startTime <= toTime) {
-      //     const transactionsResponses = await providers.subgraph.querySubgraph(
-      //       this.configs.subgraphs[i].exchange,
-      //       `
-      //       {
-      //         swaps(first: 1000, where: {timestamp_gte: ${startTime}}, orderBy: timestamp, orderDirection: asc) {
-      //           timestamp
-      //           caller
-      //           userAddress {
-      //             id
-      //           }
-      //         }
-      //       }
-      //     `
-      //     );
-      //     const transactions =
-      //       transactionsResponses && transactionsResponses['swaps'] ? transactionsResponses['swaps'] : [];
-      //     for (let i = 0; i < transactions.length; i++) {
-      //       if (transactions[i].caller && !addresses[normalizeAddress(transactions[i].caller)]) {
-      //         dateData.userCount += 1;
-      //         addresses[normalizeAddress(transactions[i].caller)] = true;
-      //       }
-      //       if (transactions[i].userAddress.id && !addresses[normalizeAddress(transactions[i].userAddress.id)]) {
-      //         dateData.userCount += 1;
-      //         addresses[normalizeAddress(transactions[i].userAddress.id)] = true;
-      //       }
-      //     }
-      //
-      //     if (transactions.length > 0) {
-      //       startTime = Number(transactions[transactions.length - 1]['timestamp']) + 1;
-      //     } else {
-      //       // no more records
-      //       break;
-      //     }
-      //   }
-      // } catch (e: any) {
-      //   logger.onDebug({
-      //     source: this.name,
-      //     message: 'failed to count daily users',
-      //     props: {
-      //       name: this.configs.name,
-      //       error: e.message,
-      //     },
-      //   });
-      // }
+    if (
+      response['startDate'] &&
+      response['startDate'].length > 0 &&
+      response['endDate'] &&
+      response['endDate'].length > 0
+    ) {
+      data.revenueUSD +=
+        Number(response['endDate'][0]['totalSwapFee']) - Number(response['startDate'][0]['totalSwapFee']);
+      data.volumeInUseUSD +=
+        Number(response['endDate'][0]['totalSwapVolume']) - Number(response['startDate'][0]['totalSwapVolume']);
+      data.transactionCount +=
+        Number(response['endDate'][0][transactionCountFilter]) -
+        Number(response['startDate'][0][transactionCountFilter]);
+      data.totalValueLockedUSD +=
+        subgraph.version === 1
+          ? new BigNumber(response['endDate'][0]['totalLiquidity']).dividedBy(1e18).toNumber()
+          : Number(response['endDate'][0]['totalLiquidity']);
     }
 
-    return dateData;
+    return data;
+  }
+
+  public async getDataInTimeFrame(providers: ShareProviders, fromTime: number, toTime: number): Promise<ProtocolData> {
+    const data: ProtocolData = {
+      revenueUSD: 0,
+      totalValueLockedUSD: 0,
+      volumeInUseUSD: 0,
+      userCount: 0,
+      transactionCount: 0,
+    };
+
+    for (const subgraph of this.configs.subgraphs) {
+      const blockNumberStartDate = await providers.subgraph.queryBlockAtTimestamp(
+        subgraph.chainConfig.subgraph?.blockSubgraph as string,
+        fromTime
+      );
+      let blockNumberEndDate = await providers.subgraph.queryBlockAtTimestamp(
+        subgraph.chainConfig.subgraph?.blockSubgraph as string,
+        toTime
+      );
+
+      // in case subgraph not full sync yet
+      const blockNumberMeta = await providers.subgraph.queryMetaLatestBlock(subgraph.exchange);
+      blockNumberEndDate = blockNumberEndDate > blockNumberMeta ? blockNumberMeta : blockNumberEndDate;
+
+      const overviewData: ProtocolData = await this.getOverviewData(
+        providers,
+        subgraph,
+        blockNumberStartDate,
+        blockNumberEndDate
+      );
+
+      data.revenueUSD += overviewData.revenueUSD;
+      data.totalValueLockedUSD += overviewData.totalValueLockedUSD;
+      data.volumeInUseUSD += overviewData.volumeInUseUSD;
+      data.userCount += overviewData.userCount;
+      data.transactionCount += overviewData.transactionCount;
+    }
+
+    return data;
   }
 }
