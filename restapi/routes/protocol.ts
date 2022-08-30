67f07c4dde08b7e3c4aaae805c4b98dbb90ae8fc
Noat <noat.eth@gmail.com>
2022-08-30 20:27:57 +0700

    restructure protocol api endpoints

--- restapi/routes/protocol.ts
+++ restapi/routes/protocol.ts
@@ -3,23 +3,19 @@ import { Router } from 'express';
 import envConfig from '../../configs/env';
 import logger from '../../lib/logger';
 import { ShareProviders } from '../../lib/types';
-import { getCache, setCache } from '../caching';
-import { removeIdFields, writeResponseData, writeResponseError } from '../helpers';
+import { writeResponseData, writeResponseError } from '../helpers';
 
 export function getRouter(providers: ShareProviders): Router {
   const router = Router({ mergeParams: true });
 
-  router.get('/:protocol', async (request, response) => {
-    const protocol = request.params.protocol || '';
+  // get protocol summarize data
+  router.get('/:protocol/summarize', async (request, response) => {
+    const protocol = request.params.protocol || null;
 
-    // query cache
-    const cacheKey = `${protocol}.data`;
-    const cacheData = getCache(cacheKey);
-    if (cacheData) {
-      writeResponseData(response, {
-        status: 200,
-        data: cacheData,
-        removeIdField: false,
+    if (!protocol) {
+      writeResponseError(response, {
+        status: 404,
+        error: 'protocol not found',
       });
       return;
     }
@@ -27,11 +23,8 @@ export function getRouter(providers: ShareProviders): Router {
     const dailyMetricCollection = await providers.database.getCollection(
       envConfig.database.collections.globalDataDaily
     );
-    const dateMetricCollection = await providers.database.getCollection(envConfig.database.collections.globalDataDate);
 
     try {
-      const data: any = {};
-
       const dailyData: Array<any> = await dailyMetricCollection
         .find({
           module: 'defi',
@@ -40,34 +33,66 @@ export function getRouter(providers: ShareProviders): Router {
         .toArray();
 
       if (dailyData.length > 0) {
-        data.daily = dailyData[0];
-      } else {
-        data.daily = null;
+        delete dailyData[0]._id;
       }
 
+      writeResponseData(response, {
+        status: 200,
+        data: dailyData[0],
+        removeIdField: false,
+      });
+    } catch (e: any) {
+      logger.onError({
+        source: 'router.protocol',
+        message: 'failed to serve api request',
+        props: {
+          path: request.path,
+          error: e.message,
+        },
+        error: e as Error,
+      });
+      writeResponseError(response, {
+        status: 500,
+        error: e.message,
+      });
+    }
+  });
+
+  // get protocol date data
+  router.get('/:protocol/datedata', async (request, response) => {
+    const protocol = request.params.protocol || null;
+
+    if (!protocol) {
+      writeResponseError(response, {
+        status: 404,
+        error: 'protocol not found',
+      });
+      return;
+    }
+
+    const dateMetricCollection = await providers.database.getCollection(envConfig.database.collections.globalDataDate);
+
+    try {
       const dateData: Array<any> = await dateMetricCollection
         .find({
           module: 'defi',
           name: protocol,
         })
+        .sort({ date: -1 })
         .toArray();
 
-      data.date = removeIdFields(dateData);
-
-      for (let i = 0; i < data.date.length; i++) {
-        if (data.date[i].detail && data.date[i].detail.tokens) {
-          delete data.date[i].detail.tokens;
+      for (let i = 0; i < dateData.length; i++) {
+        if (dateData[i].detail && dateData[i].detail.tokens) {
+          delete dateData[i].detail.tokens;
         }
       }
 
       writeResponseData(response, {
         status: 200,
-        data: data,
-        removeIdField: false,
+        data: dateData,
+        removeIdField: true,
       });
-
-      // set caching
-      setCache(cacheKey, removeIdFields(data));
+      return;
     } catch (e: any) {
       logger.onError({
         source: 'router.protocol',
